6dbba3590b12842a28e64f3892d7643e7b90968f
nkn6767 <94200097+nkn6767@users.noreply.github.com>
2024-05-28 23:07:07 -0400

    emoji code
    emoji code

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -1,7 +1,28 @@
 function setup() {
   createCanvas(400, 400);
 }
-
+// the face
 function draw() {
   background(220);
+  fill("yellow");
+  stroke("black");
+  circle(200,200,300);
+
+  //the mouth
+  fill("#000000");
+  stroke("#000000");
+  strokeWeight(1);
+  ellipse(200,250,100,100);
+
+  //the eyes
+  fill("#000000");
+  strokeWeight("20");
+  ellipse(150,155,45,45);
+
+  fill("#000000");
+  strokeWeight("20");
+  ellipse(250,155,45,45);
+  
+
+
 }
