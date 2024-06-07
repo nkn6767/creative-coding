f04d2aaa8c23022ec4622df702024aa0f0a0e235
nkn6767 <94200097+nkn6767@users.noreply.github.com>
2024-06-07 12:05:42 -0400

    Bounce Exercise

--- week3/3.1.bounce/sketch.js
+++ week3/3.1.bounce/sketch.js
@@ -1,7 +1,32 @@
+let x = 0;
+let y = 0;
+let xspeed = 5;
+let yspeed = 5;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
+  fill("orange");
+
 }
 
 function draw() {
   background(220);
+  
+  translate(width / 2, height / 2);
+
+  ellipse(x,y,50);
+
+  
+
+  if(x > 175 | x < -175){
+    xspeed = -xspeed;
+    fill(random(255),100,100);
+ }
+
+  if(y > 125 | y < -125){
+    yspeed = -yspeed;
+    fill(random(255),100,100);
+ }
+  y+= yspeed;
+  x+= xspeed; 
+    // y += 1; 
 }
