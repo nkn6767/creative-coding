277e790f1a1807087dac42588b792f06e7d2fb1c
nkn6767 <94200097+nkn6767@users.noreply.github.com>
2024-06-21 12:36:25 -0400

    love letter

--- week4/4.2.houses-of-dust/sketch.js
+++ week4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,19 @@
+let poem = '';
+
+let grammar = tracery.createGrammar({
+"origin": ["A HOUSE OF #spleck#"],
+"material" : ["MUD" ,"BRICKS", "LEAVES", "WEEDS", "STEEL"]
+});
+
 function setup() {
   createCanvas(800, 800);
+  frameRate(0.5);
 }
 
 function draw() {
   background(220);
+  poem = grammar.flatten("origin");
+  textFont("Impact");
+  textSize(50);
+  text(poem,70,200);
 }
